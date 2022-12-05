103dc3f8ccb7f191f584bfde042316f7321c7251
jarodBeardsley <jarod.beardsley@gmail.com>
2022-12-05 16:02:06 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,15 +5,16 @@ def fibonacci(n):
   """Implements Fibonacci calculation using dynamic programming for O(n) runtime"""
   a = 0
   b = 1
+  
   if n < 0:
     print("Invalid input: input must be greater than -1")
     return
-  elif n <= 1:
-    return n
-  else:
+  elif n > 1:
     for i in range(2, n+1):
       c = a + b
       a = b
       b = c
-  return b
+    n = b
+
+  return n
      
