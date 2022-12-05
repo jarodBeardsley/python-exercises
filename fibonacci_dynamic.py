82a05c797ff39a2e20756532dcf9d6c4a7bf1329
jarodBeardsley <jarod.beardsley@gmail.com>
2022-12-05 12:40:28 -0500

    Update fibonacci_dynamic.py

--- fibonacci_dynamic.py
+++ fibonacci_dynamic.py
@@ -1,12 +1,16 @@
-def fib(int n):
-  if(n<=1):
-    return n
+######################
+# Main Program       #
+######################
+if __name__ == '__main__':
+  def fib(int n):
+    if(n<=1):
+      return n
 
-  if(fib(n) != -1):
-    return fib(n)
+    if(fib(n) != -1):
+      return fib(n)
 
-  int res = fib(n-1) + fib(n-2)
+    int res = fib(n-1) + fib(n-2)
 
-  fib(n) = res
+    fib(n) = res
 
-  return res
+    return res
