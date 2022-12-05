efde2163da6327269d3e58aa5d98737251040771
jarodBeardsley <jarod.beardsley@gmail.com>
2022-12-05 13:12:39 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,19 @@
 ################
 # Main Program #
 ################
-if __name__ == '__main__':
-  def fibonacci(n):
+#Enter Python code here and hit the Run button.
+def fibonacci(n):
   """Implements Fibonacci calculation using dynamic programming for O(n) runtime"""
   a = 0
   b = 1
-    if n < 0:
-      print("Incorrect input")
-    elif n == 0:
-      return a
-    elif n == 1:
-      return b
-    else:
-      for i in range(2, n+1):
-        c = a + b
-        a = b
-        b = c
-      return b
+  if n < 0:
+    print("Invalid input")
+  elif n <= 1:
+    return n
+  else:
+    for i in range(2, n+1):
+      c = a + b
+      a = b
+      b = c
+  return b
      
