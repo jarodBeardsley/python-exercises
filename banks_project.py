a08929b1eb22bd6d641416e354109c2255529537
jarodBeardsley <jarod.beardsley@gmail.com>
2023-10-24 11:32:49 -0400

    Add files via upload
    ETL scripts in Python using Pandas for Coursera

---
+++ banks_project.py
@@ -0,0 +1,23 @@
+
+log_progress('Data transformation complete. Initiating Loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('World_Economies.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as a table. Executing queries.')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
+
+log_progress('Server Connection closed.')
