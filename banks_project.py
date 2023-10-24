cacf3957f22bf14952a5a46584a88b7299d75604
jarodBeardsley <jarod.beardsley@gmail.com>
2023-10-24 11:40:14 -0400

    Add files via upload

--- banks_project.py
+++ banks_project.py
@@ -1,3 +1,80 @@
+# Importing the required libraries
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime 
+
+# Code for ETL operations on Country-GDP data
+def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
+def extract(url, table_attribs):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
+    text = requests.get(url).text
+    data = BeautifulSoup(text, 'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables =  data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            data_dict = {"Name": col[1].contents[2],
+                "MC_USD_Billion": float(col[2].contents[0])}
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df,df1], ignore_index=True)
+    return df
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+    #dict = dataframe.set_index('Name').to_dict()['MC_USD_Billion']
+    return df
+
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+    df.to_csv(csv_path)
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+''' Here, you define the required entities and call the relevant
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Name", "MC_USD_Billion"]
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+csv_path = './Largest_banks_data.csv'
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+print(df)
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df, csv_path)
 
 log_progress('Data transformation complete. Initiating Loading process')
 
@@ -13,7 +90,13 @@ load_to_db(df, sql_connection, table_name)
 
 log_progress('Data loaded to Database as a table. Executing queries.')
 
-query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+query_statement = f"SELECT * from Largest_banks"
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM Largest_banks"
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT Name from Largest_banks LIMIT 5"
 run_query(query_statement, sql_connection)
 
 log_progress('Process Complete.')
